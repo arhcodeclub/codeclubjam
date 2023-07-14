917fc8b7005f07a64112da9ea13b4f2e733f5676
Twenmod <92715644+Twenmod@users.noreply.github.com>
2023-07-14 20:37:27 +0200

    fixed project for building

--- Assets/Gameassets/Scripts/inventory/inventorymanager.cs
+++ Assets/Gameassets/Scripts/inventory/inventorymanager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Build;
 using UnityEngine;
 
 public class inventorymanager : MonoBehaviour
